dde121ead2d72ed0aaabfb80acc55df606745f67
Andrew Rowe <andrewgrowe@gmail.com>
2018-12-03 13:21:20 +0200

    Revert "Add stylecop ruleset to sln"
    This reverts commit 3273250f279239ce4ed0a9b80623592bb12e0fbd.

--- FiveRingsDb/Controllers/CardsController.cs
+++ FiveRingsDb/Controllers/CardsController.cs
@@ -1,11 +1,11 @@
-﻿namespace FiveRingsDb.Controllers
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
-    using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
+namespace FiveRingsDb.Controllers
+{
     [Route("[controller]")]
     [ApiController]
     public class CardsController : ControllerBase
