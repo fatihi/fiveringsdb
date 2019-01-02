e6a9852f159f0d4c2953fb6175fb14432e3be8f8
Fatih Inan <fatihinan@gmail.com>
2019-01-02 09:36:44 +0100

    Add justification for suppression of stylecop ruling

--- FiveRingsDb/GlobalSuppressions.cs
+++ FiveRingsDb/GlobalSuppressions.cs
@@ -3,4 +3,4 @@
 // Project-level suppressions either have no target or are given 
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1200:Using directives must be placed correctly", Justification = "<Approved>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1200:Using directives must be placed correctly", Justification = "We agreed to that.")]
