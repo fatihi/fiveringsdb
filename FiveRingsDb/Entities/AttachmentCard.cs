d1fb5dce8c193bddcb0ff505cba11ef0083aec27
Fatih Inan <fatihinan@gmail.com>
2019-01-03 09:02:18 +0100

    Make setters in Attachment private

--- FiveRingsDb/Entities/AttachmentCard.cs
+++ FiveRingsDb/Entities/AttachmentCard.cs
@@ -2,9 +2,9 @@ namespace FiveRingsDb.Entities
 {
     public class AttachmentCard : Card
     {
-        public int Cost { get; set; }
+        public int Cost { get; private set; }
         public string MilitaryBonus { get; private set; }
         public string PoliticalBonus { get; private set; }
-        public int? InfluenceCost { get; set; }
+        public int? InfluenceCost { get; private set; }
     }
 }
