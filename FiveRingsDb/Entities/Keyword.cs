2495e08523b789337ea2e1e47d430d54687a9ace
Ben Wolf <benjamin.wolf.de@me.com>
2019-02-17 11:56:10 +0100

    Add new enum for keywords

---
+++ FiveRingsDb/Entities/Keyword.cs
@@ -0,0 +1,21 @@
+using System.Runtime.Serialization;
+
+namespace FiveRingsDb.Entities
+{
+    public enum Keyword
+    {
+        [EnumMember(Value = "ancestral")] Ancestral,
+        [EnumMember(Value = "composure")] Composure,
+        [EnumMember(Value = "courtesy")] Courtesy,
+        [EnumMember(Value = "covert")] Covert,
+        [EnumMember(Value = "disguised-bushi")] DisguisedBushi,
+        [EnumMember(Value = "disguised-cavalry")] DisguisedCavalry,
+        [EnumMember(Value = "limited")] Limited,
+        [EnumMember(Value = "no-attachments")] NoAttachments,
+        [EnumMember(Value = "no-attachments-except-weapons")] NoAttachmentsExceptWeapons,
+        [EnumMember(Value = "no-poison-attachments")] NoPoisonAttachments,
+        [EnumMember(Value = "pride")] Pride,
+        [EnumMember(Value = "restricted")] Restricted,
+        [EnumMember(Value = "sincerity")] Sincerity
+    }
+}
