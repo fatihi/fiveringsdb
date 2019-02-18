710060eb6c0503f41e6c5cf21d58765aafde66e1
Ben Wolf <benjamin.wolf.de@me.com>
2019-02-18 08:41:23 +0100

    Refactor enum to class
    We have to map complex keywords like 'No attachments' or 'Disguised'
    to our code, so we use a class that can contain either restrictions
    or exceptions to the keyword.

--- FiveRingsDb/Entities/Keyword.cs
+++ FiveRingsDb/Entities/Keyword.cs
@@ -1,23 +1,38 @@
-using System.Runtime.Serialization;
+using System.Collections.Generic;
 
 namespace FiveRingsDb.Entities
 {
-    public enum Keyword
+    /// <summary>
+    /// This class represents both simple and more complex keywords. Simple keywords only have the <see cref="Type"/>
+    /// member set. More complex keywords, like <see cref="KeywordType.NoAttachments"/> or
+    /// <see cref="KeywordType.Disguised"/>, require either additional exceptions (<see cref="Exceptions"/>) or
+    /// restrictions (<see cref="Restrictions"/>).
+    /// </summary>
+    public sealed class Keyword
     {
-        [EnumMember(Value = "ancestral")] Ancestral,
-        [EnumMember(Value = "composure")] Composure,
-        [EnumMember(Value = "courtesy")] Courtesy,
-        [EnumMember(Value = "covert")] Covert,
-        [EnumMember(Value = "disguised-bushi")] DisguisedBushi,
-        [EnumMember(Value = "disguised-cavalry")] DisguisedCavalry,
-        [EnumMember(Value = "limited")] Limited,
-        [EnumMember(Value = "no-attachments")] NoAttachments,
-        [EnumMember(Value = "no-attachments-except-monk-or-tattoo")] NoAttachmentsExceptMonkOrTattoo,
-        [EnumMember(Value = "no-attachments-except-spell")] NoAttachmentsExceptSpell,
-        [EnumMember(Value = "no-attachments-except-weapon")] NoAttachmentsExceptWeapon,
-        [EnumMember(Value = "no-poison-attachments")] NoPoisonAttachments,
-        [EnumMember(Value = "pride")] Pride,
-        [EnumMember(Value = "restricted")] Restricted,
-        [EnumMember(Value = "sincerity")] Sincerity
+        /// <summary>
+        /// Actual type of the keyword, as they are defined in the <i>Rules Reference Guide</i> by FFG (<seealso cref="KeywordType"/>).
+        /// </summary>
+        public KeywordType Type { get; set; }
+
+        /// <summary>
+        /// List of <see cref="Trait"/>s that act as an exception for the current keyword.
+        /// </summary>
+        /// <example>
+        /// The keyword <see cref="KeywordType.NoAttachments"/> can contain exceptions,
+        /// e.g. <i>No attachments except <b>Weapon</b></i>.
+        /// </example>
+        public IEnumerable<Trait> Exceptions { get; set; } = new List<Trait>();
+
+        /// <summary>
+        /// List of <see cref="Trait"/>s that act as a restriction for the current keyword.
+        /// </summary>
+        /// <example>
+        /// The keyword <see cref="KeywordType.Disguised"/> always has at least one trait that it is restricted to,
+        /// e.g. <i>Disguised <b>Bushi</b></i>.<br /><br />
+        /// In addition, the keyword <see cref="KeywordType.NoAttachments"/> can be restricted to several traits,
+        /// e.g. <i>No <b>Poison</b> attachments</i>.
+        /// </example>
+        public IEnumerable<Trait> Restrictions { get; set; } = new List<Trait>();
     }
 }
