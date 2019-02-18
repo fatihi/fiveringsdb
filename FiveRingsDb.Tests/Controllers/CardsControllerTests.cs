e69edaf179ecef65603c8ae78b5e53462613ede5
Fatih Inan <fatihinan@gmail.com>
2019-02-18 09:34:49 +0100

    WIP: Trying to fix test

--- FiveRingsDb.Tests/Controllers/CardsControllerTests.cs
+++ FiveRingsDb.Tests/Controllers/CardsControllerTests.cs
@@ -1,6 +1,11 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using FiveRingsDb.Controllers;
+using FiveRingsDb.Models;
 using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace FiveRingsDb.Tests.Controllers
@@ -12,20 +17,41 @@ namespace FiveRingsDb.Tests.Controllers
         [SetUp]
         public void Setup()
         {
-            sut = new CardsController();
+            var mockContext = SetupMock();
+            sut = new CardsController(mockContext);
         }
 
-        [Test]
-        public void GetCard_ReturnsValues()
+        private FiveRingsDbContext SetupMock()
         {
-            var result = sut.GetCard("");
-            result.Status.Should().Be(TaskStatus.RanToCompletion);
+            var mockContext = Substitute.For<FiveRingsDbContext>(new DbContextOptions<FiveRingsDbContext>());
+
+            var cards = new List<Card>
+            {
+                new EventCard {Id = "way-of-the-phoenix"},
+                new AttachmentCard {Id = "fine-katana"},
+                new StrongholdCard {Id = "isawa-mori-seido"}
+            }.AsQueryable();
+
+            var mockCards = Substitute.For<DbSet<Card>, IQueryable<Card>>();
+            ((IQueryable<Card>)mockCards).Provider.Returns(cards.Provider);
+            ((IQueryable<Card>)mockCards).Expression.Returns(cards.Expression);
+            ((IQueryable<Card>)mockCards).ElementType.Returns(cards.ElementType);
+            ((IQueryable<Card>)mockCards).GetEnumerator().Returns(cards.GetEnumerator());
+
+            return mockContext;
         }
 
         [Test]
         public void GetCards_ReturnsValues()
         {
             var result = sut.GetCards();
+            result.Status.Should().Be(TaskStatus.RanToCompletion);
+        }
+
+        [Test]
+        public void GetCard_ReturnsValues()
+        {
+            var result = sut.GetCard("");
             result.Status.Should().Be(TaskStatus.RanToCompletion);
         }
     }
