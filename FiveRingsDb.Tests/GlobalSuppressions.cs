6ac3a343907620b2d31aaadaa6f64607a7d28263
Fatih Inan <fatihinan@gmail.com>
2019-03-02 23:21:03 +0100

    Add suppressions for byte order mark rule

--- FiveRingsDb.Tests/GlobalSuppressions.cs
+++ FiveRingsDb.Tests/GlobalSuppressions.cs
@@ -3,4 +3,6 @@
 // Project-level suppressions either have no target or are given 
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1200:Using directives must be placed correctly", Justification = "We agreed to that.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Ordering Rules", "SA1200:Using directives must be placed correctly", Justification = "We agreed to that.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability Rules", "SA1214:The encoding of the file is not UTF-8 with byte order mark.", Justification = "We agreed to that.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Do not directly await a Task", Justification = "Not Relevant in .net core")]
--- FiveRingsDb/GlobalSuppressions.cs
+++ FiveRingsDb/GlobalSuppressions.cs
@@ -1,6 +1,8 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage 
+﻿// This file is used by Code Analysis to maintain SuppressMessage
 // attributes that are applied to this project.
-// Project-level suppressions either have no target or are given 
+// Project-level suppressions either have no target or are given
 // a specific target and scoped to a namespace, type, member, etc.
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1200:Using directives must be placed correctly", Justification = "We agreed to that.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Ordering Rules", "SA1200:Using directives must be placed correctly", Justification = "We agreed to that.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Maintainability Rules", "SA1214:The encoding of the file is not UTF-8 with byte order mark.", Justification = "We agreed to that.")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Reliability", "CA2007:Do not directly await a Task", Justification = "Not Relevant in .net core")]
