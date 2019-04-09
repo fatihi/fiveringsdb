37fdf78112e279a41a9b4a2c821b698f914c61dc
xWorkerBeex <martinschroeder13@yahoo.de>
2019-04-09 13:40:37 +0200

    Made Cost nullable and changed "any" element to "all"

--- FiveRingsDb/Models/EventCard.cs
+++ FiveRingsDb/Models/EventCard.cs
@@ -2,7 +2,7 @@ namespace FiveRingsDb.Models
 {
     public class EventCard : Card
     {        
-        public int Cost { get; set; }
+        public int? Cost { get; set; }
 
         public int? InfluenceCost { get; set; }
     }
