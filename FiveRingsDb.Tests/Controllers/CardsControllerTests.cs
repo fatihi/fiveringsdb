3f7cc33fad8e79f66d70594b8b49008230fa80b6
Fatih Inan <fatihinan@gmail.com>
2019-04-20 19:57:27 +0200

    Fix second test

--- FiveRingsDb.Tests/Controllers/CardsControllerTests.cs
+++ FiveRingsDb.Tests/Controllers/CardsControllerTests.cs
@@ -13,21 +13,33 @@ namespace FiveRingsDb.Tests.Controllers
 {
     public class CardsControllerTests
     {
-        private CardsController sut;
-
-        [SetUp]
-        public void Setup()
-        {
-            ICardsRepository mockRepository = SetupRepository();
-            sut = new CardsController(mockRepository);
-        }
-
-        private static ICardsRepository SetupRepository()
+        [Test]
+        public async Task GetCards_ReturnsValues()
         {
             var cardsList = CreateMockCardsList();
             var mockRepository = Substitute.For<ICardsRepository>();
             mockRepository.GetCards().Returns(cardsList);
-            return mockRepository;
+            var controller = new CardsController(mockRepository);
+
+            var result = await controller.GetCards() as OkObjectResult;
+
+            var cards = result.Value as List<Card>;
+            cards.Count.Should().Be(1);
+            cards[0].Id.Should().Be("way-of-the-phoenix");
+        }
+
+        [Test]
+        public async Task GetCard_ReturnsValue()
+        {
+            const string cardId = "way-of-the-phoenix";
+            var mockRepository = Substitute.For<ICardsRepository>();
+            mockRepository.GetCard(cardId).Returns(new EventCard { Id = cardId });
+            var controller = new CardsController(mockRepository);
+
+            var result = await controller.GetCard(cardId) as OkObjectResult;
+
+            var card = result.Value as Card;
+            card.Id.Should().Be(cardId);
         }
 
         private static IList<Card> CreateMockCardsList()
@@ -39,23 +51,6 @@ namespace FiveRingsDb.Tests.Controllers
                     Id = "way-of-the-phoenix"
                 }
             };
-        }
-
-        [Test]
-        public async Task GetCards_ReturnsValues()
-        {
-            var result = await sut.GetCards() as OkObjectResult;
-
-            var cards = result.Value as List<Card>;
-            cards.Count.Should().Be(1);
-            cards[0].Id.Should().Be("way-of-the-phoenix");
-        }
-
-        [Test]
-        public void GetCard_ReturnsValue()
-        {
-            var result = sut.GetCard("");
-            result.Status.Should().Be(TaskStatus.RanToCompletion);
         }
     }
 }
