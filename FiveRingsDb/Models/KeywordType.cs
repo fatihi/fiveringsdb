2ec7dd29aeb8d29cf06b0573ea88a09a9b30b11c
Fatih Inan <fatihinan@gmail.com>
2019-06-17 10:07:19 +0200

    Work on enum warnings.

--- FiveRingsDb/Models/KeywordType.cs
+++ FiveRingsDb/Models/KeywordType.cs
@@ -2,26 +2,76 @@ using System.Runtime.Serialization;
 
 namespace FiveRingsDb.Models
 {
+    /// <summary>
+    /// Types of Keywords.
+    /// </summary>
     public enum KeywordType
     {
+        /// <summary>
+        /// Ancestral is a keyword found on attachments.
+        /// When the card to which the ancestral card is attached to leaves play, then it is returned to its owner's hand instead of being discarded as well.
+        /// </summary>
         [EnumMember(Value = "ancestral")]
         Ancestral,
+
+        /// <summary>
+        /// Composure is a keyword found on characters.
+        /// A character with this keyword gains an additional ability while its controller's honor bid is lower than that of one of his or her opponent.
+        /// </summary>
         [EnumMember(Value = "composure")]
         Composure,
+
+        /// <summary>
+        /// Courtesy is a keyword found on characters.
+        /// When a character with courtesy leaves play, then the owner gains 1 fate.
+        /// </summary>
         [EnumMember(Value = "courtesy")]
         Courtesy,
+
+        /// <summary>
+        /// Covert is a keyword found on characters.
+        /// When this character is declared as an attacker in a conflict, you may choose a character that does not have covert. That character cannot be declared as a defender for this conflict during the "declare defenders" step.
+        /// </summary>
         [EnumMember(Value = "covert")]
         Covert,
+
+        /// <summary>
+        /// Disguised is a keyword found on characters.
+        /// At any time you could play a character from your hand during the conflict phase, choose any non-unique character that matches the restrictions listed on the disguised character. Put the disguised character into play, unbowed, in the same location that the chosen character is, reducing the cost to play the disguised character from your province or your hand by the printed cost of the chosen character. The disguised character can't have additional fate put on them when they're played in this way. Move all attachments and tokens (including fate tokens and status tokens) from the chosen character to the disguised character. Discard the chosen character from play.
+        /// </summary>
         [EnumMember(Value = "disguised")]
         Disguised,
+
+        /// <summary>
+        /// A player cannot play more than one limited card per round.
+        /// </summary>
         [EnumMember(Value = "limited")]
         Limited,
+
+        /// <summary>
+        /// A character with the No Attachments keyword cannot have any attachments.
+        /// </summary>
         [EnumMember(Value = "no-attachments")]
         NoAttachments,
+
+        /// <summary>
+        /// Pride is a keyword found on characters.
+        /// A character with this keyword gets honored when winning a conflict and dishonored when losing a conflict.
+        /// </summary>
         [EnumMember(Value = "pride")]
         Pride,
+
+        /// <summary>
+        /// Restricted is a keyword found on attachments.
+        /// A character cannot equip more than two restricted attachments.
+        /// </summary>
         [EnumMember(Value = "restricted")]
         Restricted,
+
+        /// <summary>
+        /// Sincerity is a keyword found on characters.
+        /// When a character with this keyword leaves play, then the owner draws 1 card.
+        /// </summary>
         [EnumMember(Value = "sincerity")]
         Sincerity
     }
--- FiveRingsDb/Models/Side.cs
+++ FiveRingsDb/Models/Side.cs
@@ -2,14 +2,32 @@
 
 namespace FiveRingsDb.Models
 {
+    /// <summary>
+    /// Places that cards can be placed on the field.
+    /// </summary>
     public enum Side
     {
+        /// <summary>
+        /// Represents cards in the conflict deck.
+        /// </summary>
         [EnumMember(Value = "conflict")]
         Conflict,
+
+        /// <summary>
+        /// Represents cards in the provinces. This includes strongholds.
+        /// </summary>
         [EnumMember(Value = "province")]
         Province,
+
+        /// <summary>
+        /// Represents cards in the dynasty deck.
+        /// </summary>
         [EnumMember(Value = "dynasty")]
         Dynasty,
+
+        /// <summary>
+        /// Represents role cards.
+        /// </summary>
         [EnumMember(Value = "role")]
         Role
     }
