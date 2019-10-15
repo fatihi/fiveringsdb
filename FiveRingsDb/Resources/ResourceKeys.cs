c2bd3f247515b691629d0c121df469eeb113cdc1
Fatih Inan <fatihinan@gmail.com>
2019-10-15 11:24:18 +0200

    Translate Layout file

--- FiveRingsDb/Resources/ResourceKeys.cs
+++ FiveRingsDb/Resources/ResourceKeys.cs
@@ -1,11 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace FiveRingsDb.Resources
+﻿namespace FiveRingsDb.Resources
 {
-    public class ResourceKeys
+    public static class ResourceKeys
     {
+        public const string Cards = "Cards";
+        public const string Decks = "Decks";
+        public const string Builder = "Builder";
+        public const string Rules = "Rules";
+        public const string WorkInProgress = "WorkInProgress";
+        public const string ApiDoc = "ApiDoc";
+        public const string Disclaimer = "Disclaimer";
+        public const string Copyright = "Copyright";
     }
 }
